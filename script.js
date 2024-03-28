39cd65e2363fe7c44a8bb398ee5b4ccb10711a9e
gitdishant <92519667+gitdishant@users.noreply.github.com>
2024-03-28 18:56:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+const student = {
+  name: 'John Doe',
+};
+
+// Add the getKeys() method to the Object prototype
+Object.prototype.getKeys = function () {
+  return Object.keys(this);
+};
+
+// Example usage:
+console.log(student.getKeys());
